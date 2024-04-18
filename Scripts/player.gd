4814cb72a6c203a6f3b1f924492c85ff325f72b9
David Silva <davidsilvafsa16@gmail.com>
2024-04-18 00:01:53 -0300

    Adição da animação de tiro do personagem(atirando e andando)

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -9,6 +9,8 @@ const JUMP_FORCE = -300.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var is_jumping := false
+var is_shooting := false
+var direction
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -17,23 +19,36 @@ func _physics_process(delta):
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_FORCE
+		velocity.y = JUMP_FORCE 
 		is_jumping = true
 	elif is_on_floor():
 		is_jumping = false
-
+		
+	if Input.is_action_pressed("fire"):
+		is_shooting = true
+	else:
+		is_shooting = false
+		
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("left_move", "rigth_move")
+	direction = Input.get_axis("left_move", "rigth_move")
 	if direction:
 		velocity.x = direction * SPEED
 		animation.scale.x = direction * 3
-		if !is_jumping:
-			animation.play("correndo")
-	elif is_jumping:
-		animation.play("pulando")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		animation.play("parado")
-
+			
 	move_and_slide()
+	handle_animation()
+	
+func handle_animation():
+	if !is_on_floor() and !is_shooting:
+		animation.play("pulando")
+	elif direction and !is_shooting:
+		animation.play("correndo")
+	elif !direction and is_shooting:
+		animation.play("atirar")
+	elif is_shooting and direction:
+		animation.play("atirar_correndo")
+	else:
+		animation.play("parado")
