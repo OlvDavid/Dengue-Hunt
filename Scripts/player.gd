72b6b0c862678d88b595a382da3f0721d8b102c0
David Silva <davidsilvafsa16@gmail.com>
2024-04-20 08:29:11 -0300

    Adição de novos assets a montagem do cenario

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,35 +2,36 @@ extends CharacterBody2D
 
 
 const SPEED = 200.0
-const JUMP_FORCE = -300.0
+const JUMP_FORCE = -350.0
 
 @onready var animation := $anim as AnimatedSprite2D
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
+#Variaveis globais do Projeto
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var is_jumping := false
 var is_shooting := false
 var direction
+var is_crounching := false
 
 func _physics_process(delta):
-	# Add the gravity.
+	# Fisica do Personagem
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	# Handle jump.
+	# Acionar Pular
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_FORCE 
 		is_jumping = true
 	elif is_on_floor():
 		is_jumping = false
-		
+	
+	#Acionar Tiro	
 	if Input.is_action_pressed("fire"):
 		is_shooting = true
 	else:
 		is_shooting = false
 		
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	#Movimentação do Personagem(Direita e esquerda)
 	direction = Input.get_axis("left_move", "rigth_move")
 	if direction:
 		velocity.x = direction * SPEED
