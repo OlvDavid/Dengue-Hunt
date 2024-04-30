f601b98882275f6132c9920c3d2810578cd33db7
David Silva <davidsilvafsa16@gmail.com>
2024-04-30 13:45:10 -0300

    Complemento do inimigo patrulha

---
+++ Scripts/enemy.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+
+const SPEED = 700.0
+const JUMP_VELOCITY = -400.0
+
+@onready var wall_detector := $wall_detector as RayCast2D
+@onready var texture := $texture as Sprite2D
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var direction := -1
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		
+	if wall_detector.is_colliding():
+		direction *= -1
+		wall_detector.scale.x *= -1
+		texture.scale.x *= -1
+
+	velocity.x = direction * SPEED * delta
+	
+
+	move_and_slide()
