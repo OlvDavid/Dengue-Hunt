06e27838209b705987aee2909e8d0031bc380944
OlvDavid <146491742+OlvDavid@users.noreply.github.com>
2024-05-15 14:30:42 -0300

    Revert "Tela de Game over"
    This reverts commit f93059c0628096d45607d9fbe8b43dcac561c629.

--- Scripts/world_01.gd
+++ Scripts/world_01.gd
@@ -5,13 +5,11 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
 	player.follow_camera(camera)
-	player.player_has_died.connect(game_over)
+	player.player_has_died.connect(reload_game)
 	Globals.player_life = 3
-	
-	
-	
-func game_over():
-	get_tree().change_scene_to_file("res://scenes/game_over.tscn")
+
+func reload_game():
+	await get_tree().create_timer(1.0).timeout
+	get_tree().reload_current_scene()
 	
